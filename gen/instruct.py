38a7825fcde960592c07b8cadc3e93284296a2d4
Manish Prakash <manish.prakash@citymall.live>
2024-03-01 08:40:27 +0530

    init

--- gen/instruct.py
+++ gen/instruct.py
@@ -7,6 +7,28 @@ from transformers import AutoTokenizer
 import vllm
 from datasets import Dataset
 import torch
+import random
+
+
+from system_messages import (
+    SYSTEM_MESSAGES_ORCA,
+    SYSTEM_MESSAGES_TESS,
+    SYSTEM_MESSAGES_CODE,
+)
+from topics import TOPICS
+
+code_data = False
+
+if code_data:
+    SYSTEM_MESSAGES = SYSTEM_MESSAGES_CODE
+    PROMPT_1 = """For the following SUBJECT_AREA, generate a question that covers a very narrow topic in the SUBJECT_AREA, with sufficient depth and breadth. The topic in the question should be important to the SUBJECT_AREA, with known-answers present. The generated question should be detailed, seek true nature of our universe from first principles, curiosity invoking, thought provoking, and also should be able to be answered by an intelligence like yourself. Make sure the question is sufficiently harder and multi-part, like a graduate level course question. The question should seek an answer with computer code."""
+
+else:
+    SYSTEM_MESSAGES = SYSTEM_MESSAGES_ORCA + SYSTEM_MESSAGES_TESS
+    PROMPT_1 = """For the following SUBJECT_AREA, generate a question that covers a very narrow topic in the SUBJECT_AREA, with sufficient depth and breadth. The topic in the question should be important to the SUBJECT_AREA, with known-answers present. The generated question should be detailed, seek true nature of our universe from first principles, curiosity invoking, thought provoking, and also should be able to be answered by an intelligence like yourself. Make sure the question is sufficiently harder and multi-part, like a graduate level course question."""
+
+
+msg_context = {"role": "system", "content": str(PROMPT_1)}
 
 
 @torch.no_grad()
@@ -28,19 +50,42 @@ def eval_hf_model(args, model, tokenizer, prompts):
     return outputs
 
 
+def generate_data(
+    topic_selected,
+    system_message_generation,
+    system_message_selected,
+):
+    system_contexts = [
+        system_message_generation,
+        system_message_selected,
+    ]
+
+    user_prompts = [f"SUBJECT_AREA: {topic_selected}"]
+    gpt_outputs = []
+
+    for pp in range(len(system_contexts)):
+        msg_list = []
+        msg_system = {"role": "system", "content": str(system_contexts[pp])}
+        msg_list.append(msg_system)
+        msg_prompt = {"role": "user", "content": user_prompts[pp]}
+        msg_list.append(msg_prompt)
+
+        print(msg_prompt)
+
+
 def main(args):
 
-    base_repo = "manishiitg/aditi-dpo-prompts"
-    dataset = load_dataset(base_repo, split="train")
-    dataset = dataset.filter(lambda x: x["language"] == args.lang)
-    dataset = dataset.select(range(10))
+    topic_number = random.randint(0, len(TOPICS))
+    topic_selected = TOPICS[topic_number]
+    system_message_number = random.randint(0, len(SYSTEM_MESSAGES))
+    system_message_selected = SYSTEM_MESSAGES[system_message_number]
+    system_message_generation = PROMPT_1
 
-    final_data = []
-
-    for row in dataset:
-        processed_by = row["processed_by"]
-        if args.model_name_or_path not in processed_by:
-            final_data.append(row)
+    generate_data(topic_selected,
+                  system_message_generation,
+                  system_message_selected)
+    
+    os.exit(1)
 
     tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
 
