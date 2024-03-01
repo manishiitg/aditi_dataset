43f21835de2d38c8dc49a6cd3d4f74716c9610ba
Manish Prakash <manish.prakash@citymall.live>
2024-03-01 13:02:49 +0530

    init

--- judge/dpo/run.py
+++ judge/dpo/run.py
@@ -41,7 +41,7 @@ def main(args):
         processed_by = row["processed_by"]
         if args.model_name_or_path not in processed_by:
             final_data.append(row)
-        if args.model_name_or_path in processed_by and processed_by[args.model_name_or_path]:
+        if args.model_name_or_path in processed_by and not processed_by[args.model_name_or_path]:
             final_data.append(row)
         if len(final_data) > max_rows:
             break
