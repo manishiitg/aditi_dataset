d4b50ade95e07e58dcd1c8c7c030b5e2cd65748e
Manish Prakash <manish.prakash@citymall.live>
2024-03-01 07:45:55 +0530

    init

---
+++ judge/dpo/run.py
@@ -0,0 +1,132 @@
+from tqdm import tqdm
+import argparse
+import os
+import torch
+import json
+from datasets import load_dataset
+from transformers import AutoTokenizer
+import vllm
+from datasets import Dataset
+import torch
+import re
+import time
+
+
+@torch.no_grad()
+def eval_hf_model(args, model, tokenizer, prompts):
+    sampling_params = vllm.SamplingParams(
+        temperature=0,
+        max_tokens=512,
+        stop=["<|im_end|>"],
+    )
+    # We need to remap the outputs to the prompts because vllm might not return outputs for some prompts (e.g., if the prompt is too long)
+    generations = model.generate(prompts, sampling_params)
+
+    prompt_to_output = {
+        g.prompt: g.outputs[0].text.strip() for g in generations
+    }
+    outputs = [prompt_to_output[prompt]
+               if prompt in prompt_to_output else "" for prompt in prompts]
+
+    return outputs
+
+
+def main(args):
+
+    base_repo = "manishiitg/aditi-dpo-prompts"
+    dataset = load_dataset(base_repo, split="train")
+    dataset = dataset.filter(lambda x: x["language"] == args.lang)
+    dataset = dataset.select(range(100))
+
+    final_data = []
+
+    for row in dataset:
+        processed_by = row["processed_by"]
+        if args.model_name_or_path not in processed_by:
+            final_data.append(row)
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        args.tokenizer_name_or_path if args.tokenizer_name_or_path else args.model_name_or_path)
+
+    if args.awq:
+        print("Loading model and tokenizer vllm awq...")
+        model = vllm.LLM(
+            model=args.model_name_or_path,
+            tokenizer=args.tokenizer_name_or_path if args.tokenizer_name_or_path else args.model_name_or_path,
+            tokenizer_mode="auto",
+            tensor_parallel_size=torch.cuda.device_count(),
+            quantization="AWQ",
+            max_model_len=8196,
+        )
+    else:
+        print("Loading model and tokenizer vllm...")
+        model = vllm.LLM(
+            model=args.model_name_or_path,
+            tokenizer=args.tokenizer_name_or_path if args.tokenizer_name_or_path else args.model_name_or_path,
+            tokenizer_mode="auto",
+            tensor_parallel_size=torch.cuda.device_count(),
+            max_model_len=8196,
+        )
+
+    prompts = []
+    pending_data = []
+    for row in tqdm(final_data):
+
+        prompt = row["prompt"]
+        messages = [
+            {"role": "user", "content": prompt}
+        ]
+        text = tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True
+        )
+        prompts.append(text)
+
+    outputs = eval_hf_model(args, model, tokenizer, prompts)
+
+    for idx, text in enumerate(outputs):
+        pending_data[idx] = final_data[idx]
+        processed_by = pending_data[idx]["processed_by"]
+        processed_by[args.model_name_or_path] = True
+        ratings = pending_data[idx]["ratings"]
+        ratings[args.model_name_or_path] = text
+
+        pass
+
+    existing_data = []
+    dataset = load_dataset(base_repo, split="train")
+    for r in dataset:
+        processed_by = row["processed_by"]
+        if args.model_name_or_path not in processed_by:
+            pass
+        else:
+            existing_data.append(row)
+
+    final_data = pending_data + existing_data
+    dataset = process_and_update_dataset(final_data)
+    dataset.push_to_hub("manishiitg/custom-data-chat", private=False)
+
+
+def process_and_update_dataset(new_data):
+    new_data_formatted = {key: [item[key]
+                                for item in new_data] for key in new_data[0].keys()}
+    new_dataset_chunk = Dataset.from_dict(new_data_formatted)
+    dataset2 = new_dataset_chunk
+    return dataset2
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    parser.add_argument(
+        "--model_name_or_path",
+        type=str,
+        default=None,
+    )
+    parser.add_argument(
+        "--lang",
+        type=str,
+        default=None,
+    )
+    main(args)
---
+++ scripts/dpo.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+source ./scripts/indic_eval/common_vars.sh
+
+TASK_NAME=dpo
+
+for model_name_or_path in "${model_names[@]}"; do
+    model_name=${model_name_or_path##*/}
+    
+    echo "evaluating $model_name base on $TASK_NAME $NUM_SHOTS ..."
+
+    if echo "$model_name" | grep -qi "awq"; then
+        awq_param="--awq"
+    else
+        awq_param=""
+    fi
+    
+    python3 -m eval.dpo.run \
+        --model_name_or_path $model_name_or_path \
+        --lang hi
+        $awq_param
+    
+done
+
+for model_name_or_path in "${model_names[@]}"; do
+    model_name=${model_name_or_path##*/}
+    
+    echo "evaluating $model_name base on $TASK_NAME $NUM_SHOTS ..."
+
+    if echo "$model_name" | grep -qi "awq"; then
+        awq_param="--awq"
+    else
+        awq_param=""
+    fi
+    
+    python3 -m eval.dpo.run \
+        --model_name_or_path $model_name_or_path \
+        --lang en
+        $awq_param
+    
+done
