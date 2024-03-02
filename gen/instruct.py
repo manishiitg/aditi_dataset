b27c8b83b9f94bccaea6de7d4e46d5ea3f9df945
Manish Prakash <manish.prakash@citymall.live>
2024-03-02 16:18:48 +0530

    init

--- gen/instruct.py
+++ gen/instruct.py
@@ -270,6 +270,7 @@ def main(args):
 
             prompts = []
             topics_selected = []
+            questions = []
             random.shuffle(TOPICS)
             for topic_selected in TOPICS:
 
@@ -294,6 +295,7 @@ def main(args):
                     msg_list.append(msg_system)
                     msg_prompt = {"role": "user", "content": user}
                     msg_list.append(msg_prompt)
+                    
                     text = tokenizer.apply_chat_template(
                         msg_list,
                         tokenize=False,
@@ -320,6 +322,7 @@ def main(args):
                                   "content": system_message_selected}
                     msg_list.append(msg_system)
                     msg_prompt = {"role": "user", "content": text}
+                    questions.append(text)
                     msg_list.append(msg_prompt)
                     text = tokenizer.apply_chat_template(
                         msg_list,
@@ -338,7 +341,7 @@ def main(args):
                     print("text", text)
                     final_data.append({
                         "topic": topics_selected[idx],
-                        "question": prompts[idx],
+                        "question": questions[idx],
                         "answer": text,
                         "system_prompt": sys_prompt_selected[idx],
                         "language": args.lang,
