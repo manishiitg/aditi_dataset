9bf5c0137933a7a489d6bfedb69295a370d4a083
Manish Prakash <manish.prakash@citymall.live>
2024-03-03 21:18:01 +0530

    init

--- gen/amplify.py
+++ gen/amplify.py
@@ -112,7 +112,7 @@ def main(args):
 
     base_repo = "manishiitg/indic-synthetic-instruct"
 
-    max_rows = 100
+    max_rows = 50
     final_data = []
     existing_data = []
     existing_ds = load_dataset(base_repo, split="train")
