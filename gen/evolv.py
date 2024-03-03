9f7036e33668441e5101704f75434e7a3add2ab1
Manish Prakash <manish.prakash@citymall.live>
2024-03-03 10:22:09 +0530

    init

--- gen/evolv.py
+++ gen/evolv.py
@@ -11,15 +11,37 @@ import random
 import re
 
 
-base_instruction = """
-    Based on the conversion between an ai assistant and user, generate next possible question which a user can ask.
+base_instruction = "I want you act as a Prompt Rewriter.\r\n \
+					Your objective is to rewrite a given prompt into a more complex version to make those famous AI systems (e.g., chatgpt and GPT4) a bit harder to handle.\r\n \
+					But the rewritten prompt must be reasonable and must be understood and responded by humans.\r\n \
+					Your rewriting cannot omit the non-text parts such as the table and code in #The Given Prompt#:. Also, please do not omit the input in #The Given Prompt#. \r\n \
+					You SHOULD complicate the given prompt using the following method: \r\n\
+					{} \r\n\
+					You should try your best not to make the #Rewritten Prompt# become verbose, #Rewritten Prompt# can only add 10 to 20 words into #The Given Prompt#. \r\n\
+					'#The Given Prompt#', '#Rewritten Prompt#', 'given prompt' and 'rewritten prompt' are not allowed to appear in #Rewritten Prompt#\r\n"
 
-"""
+
+def createConstraintsPrompt(instruction):
+    prompt = base_instruction.format(
+        "Please add one more constraints/requirements into #The Given Prompt#'")
+    prompt += "#The Given Prompt#: \r\n {} \r\n".format(instruction)
+    prompt += "#Rewritten Prompt#:\r\n"
+    return prompt
 
 
-def createDeepenPrompt():
-    prompt = base_instruction + \
-        "If the given conversation contains inquiries about certain issues, the depth and breadth of the inquiry can be increased."
+def createDeepenPrompt(instruction):
+    prompt = base_instruction.format(
+        "If #The Given Prompt# contains inquiries about certain issues, the depth and breadth of the inquiry can be increased.")
+    prompt += "#The Given Prompt#: \r\n {} \r\n".format(instruction)
+    prompt += "#Rewritten Prompt#:\r\n"
+    return prompt
+
+
+def createConcretizingPrompt(instruction):
+    prompt = base_instruction.format(
+        "Please replace general concepts with more specific concepts.")
+    prompt += "#The Given Prompt#: \r\n {} \r\n".format(instruction)
+    prompt += "#Rewritten Prompt#:\r\n"
     return prompt
 
 
@@ -87,19 +109,16 @@ def main(args):
     for row in final_data:
 
         messages = []
-        messages.append({"role": "system", "content": row["system_prompt"]})
+        # messages.append({"role": "system", "content": row["system_prompt"]})
         messages.append({"role": "user", "content": row["question"]})
-        messages.append({"role": "assistant", "content": row["answer"]})
+        # messages.append({"role": "assistant", "content": row["answer"]})
 
         instruction = ""
         for r in messages:
-            instruction += r["role"] + ":" + r["content"] + "\n\n"
+            instruction += "\n\n" + r["content"]
 
-        system = createDeepenPrompt()
-        msg_list = [
-            {"role": "system", "content": system},
-            {"role": "user", "content": instruction}
-        ]
+        instruction = createConstraintsPrompt(instruction)
+        msg_list = [{"role": "user", "content": instruction}]
 
         text = tokenizer.apply_chat_template(
             msg_list,
