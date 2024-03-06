aa4041d667c268ef495b53033a9eb2bd7b8455e0
Manish Prakash <manish.prakash@citymall.live>
2024-03-06 09:45:39 +0530

    init

--- gen/instruct-alpaca.py
+++ gen/instruct-alpaca.py
@@ -231,10 +231,10 @@ def main(args):
             if args.generate_topics:
                 message = []
                 prompt = """
-                    Give me a numbered list of 50 completely random topics.
+                    Give me a numbered list of 50 completely random topics  , related to india, indian culture, indian socity, latest trends in india and what people talk about in india
                     Generate a diverse list of topics in english.
                 """
-                # , related to india, indian culture, indian socity, latest trends in india and what people talk about in india
+                #
                 if len(topics_generated) > 0:
                     prompt += "\n Topics should not be related to " + \
                         ",".join(topics_generated)
