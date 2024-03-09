4620cde78d128822c6a74f7723ac63654541ffdb
Manish Prakash <manish.prakash@citymall.live>
2024-03-09 15:22:52 +0530

    init

--- scripts/samantha.sh
+++ scripts/samantha.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 model_names=(
-    "Qwen/Qwen1.5-72B-Chat-AWQ"
+    "Qwen/Qwen1.5-7B-Chat-AWQ"
 )
 FOLDER_BASE=/sky-notebook/eval-results
 
@@ -9,7 +9,7 @@ TASK_NAME=gen
 
 for model_name_or_path in "${model_names[@]}"; do
     model_name=${model_name_or_path##*/}
-    
+
     if echo "$model_name" | grep -qi "awq"; then
         awq_param="--awq"
     else
