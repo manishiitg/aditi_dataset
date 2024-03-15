3b1d901a07403e82911a39d1ff924033c20c49dc
Manish Prakash <manish.prakash@citymall.live>
2024-03-15 07:55:49 +0530

    working on agents dataset

--- agent/instruct-agent-sim.py
+++ agent/instruct-agent-sim.py
@@ -97,7 +97,7 @@ When replying use the following format
 Thought in English: think step by step about what to do in detail.
 Action: the action to take if you have all tool parameter values, only one name of [{tool_names}], in the exact format {'arguments': <args-dict>, 'name': <function-name>}
 Should Execute Action: do we have all parameter values to execute action reply only yes or no. If yes, i.e executing a tool reply to user should only contain a message asking user to wait.
-Reply to User In {language}: a short natural language based message to be sent to the user only in {language}. Don't generate english translation of the reply.
+Reply to User In {language}: a short natural language based message to be sent to the user only in {language}. Don't write english translation of the answer.
 """
 
 AGENT_PROMPT_USER_SIMULATION = """
