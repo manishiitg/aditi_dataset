e97b7763d0181118c0478a5c787643a6f311a020
Manish Prakash <manish.prakash@citymall.live>
2024-03-17 19:15:06 +0530

    update readme

--- gen/instruct-alpaca.py
+++ gen/instruct-alpaca.py
@@ -222,7 +222,7 @@ def main(args):
     for lang in languages:
         args.lang = lang
         topic_instruct_map = {}
-        for loop in range(4):
+        for loop in range(10):
 
             prompts = []
             topics_selected = []
