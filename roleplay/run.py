464bbc1e340481985ae60b7c59d823487e0ca167
Manish Prakash <manish.prakash@citymall.live>
2024-03-23 18:29:28 +0530

    update readme

--- roleplay/run.py
+++ roleplay/run.py
@@ -180,7 +180,7 @@ def main(args):
                 if len(existing_instruction) > 0:
                     USER_PROMPT += "\n\n" + "Generated Tasks should be different from " + existing_instruction
 
-            user = USER_PROMPT.replace("{batch_size}", "20")
+            user = USER_PROMPT.replace("{batch_size}", "50")
             SYSTEM_PROMPT = "You are an helpful AI assistant"
             msg_system = {"role": "system", "content": SYSTEM_PROMPT}
             msg_list.append(msg_system)
