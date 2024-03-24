b3b5e232cbc80f432c15d3d434d2152ee56634d3
Manish Prakash <manish.prakash@citymall.live>
2024-03-24 10:11:25 +0530

    working on new datasets

--- scripts/writing.sh
+++ scripts/writing.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 model_names=(
-    "Qwen/Qwen1.5-72B-Chat-AWQ"
-    # "MaziyarPanahi/Smaug-72B-v0.1-AWQ"
+    # "Qwen/Qwen1.5-72B-Chat-AWQ"
+    "MaziyarPanahi/Smaug-72B-v0.1-AWQ"
 )
 FOLDER_BASE=/sky-notebook/eval-results
 
