ed5eb182d6aee9691ea2d859d070f01a653bfda4
Manish Prakash <manish.prakash@citymall.live>
2024-03-24 17:11:58 +0530

    working on new datasets

--- orca/run.py
+++ orca/run.py
@@ -354,7 +354,7 @@ def main(args):
         if args.generate_topics or True:
             message = []
             prompt = """
-                Give me a numbered list of 50 completely random topics related to common sense reasoning.
+                Give me a numbered list of 50 completely random topics related to reasoning.
                 Generate a diverse list of topics in english.
             """
 
