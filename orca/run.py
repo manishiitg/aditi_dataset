e9c1464e9cae12ca839e42e7e74e9febe8c7ba79
Manish Prakash <manish.prakash@citymall.live>
2024-03-24 19:38:07 +0530

    working on new datasets

--- orca/run.py
+++ orca/run.py
@@ -297,7 +297,7 @@ ANSWER: [second question's answer in hinglish]
 def eval_hf_model(args, model, tokenizer, prompts, temperature):
     sampling_params = vllm.SamplingParams(
         temperature=temperature,
-        max_tokens=2048,
+        max_tokens=4096,
         stop=["<|im_end|>"],
     )
     # We need to remap the outputs to the prompts because vllm might not return outputs for some prompts (e.g., if the prompt is too long)
