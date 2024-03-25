8a1d3898ebe33dbdd8ed0822358735c1cb0aa4d7
Manish Prakash <manish.prakash@citymall.live>
2024-03-25 10:28:40 +0530

    working on new datasets

--- rag/run.py
+++ rag/run.py
@@ -348,7 +348,6 @@ def main(args):
                     add_generation_prompt=True
                 )
                 prompts2.append(text)
-                print("prompt2", text)
 
             max_tokens = 2048
             if lang == "hindi":
