3332a6004dac9663fe748d8e9aa759835b7f9429
Manish Prakash <manish.prakash@citymall.live>
2024-03-31 15:27:24 +0530

    working on new datasets

--- scripts/judge.sh
+++ scripts/judge.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
 TASK_NAME=judge
-
-for model_name_or_path in "${model_names[@]}"; do
-    model_name=${model_name_or_path##*/}
-    
-    echo "evaluating $model_name base on $TASK_NAME $NUM_SHOTS ..."
-
-    if echo "$model_name" | grep -qi "awq"; then
-        awq_param="--awq"
-    else
-        awq_param=""
-    fi
-    
-    cd judge/dataset_eval
-    python3 judge-instruct.py
-    cd ..
-    
-done
+echo "evaluating $model_name base on $TASK_NAME $NUM_SHOTS ..."
+cd judge/dataset_eval
+python3 judge-instruct.py
+cd ..
