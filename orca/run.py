712c8ea877b30db3117ffa574f0e31e25ce890cb
Manish Prakash <manish.prakash@citymall.live>
2024-04-05 19:29:35 +0530

    working on new datasets

--- orca/run.py
+++ orca/run.py
@@ -356,7 +356,7 @@ def main(args):
             topics_generated = topics_generated_map[lang]
         topic_instruct_map = {}
 
-        for loop in range(10):
+        for loop in range(20):
             prompts = []
             if args.generate_topics or True:
                 message = []
